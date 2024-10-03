b58d6332ca7fa0810caf2d7ae851d141a56937e8
berlily29 <yambaoluise29@gmail.com>
2024-10-03 16:34:59 +0800

    database

---
+++ eclc/database/migrations/2024_10_03_082429_users.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ECLC_INPUTS', function (Blueprint $table) {
+            $table->id();
+            $table->integer('ep_id', 11)->unique();
+            $table->integer('user_id', 11)->unique();
+            $table->string('noe', 255);
+            $table->date('date_not');
+            $table->string('Type_PI',255);
+            $table->string('noc', 255);
+            $table->string('des_out', 255);
+            $table->string('dop', 255);
+            $table->string('tar_ben', 255);
+            $table->string('wwp', 255);
+            $table->date('expiry_date');
+            $table->String('E_NE',255);
+            $table->String('MOA',255);
+            $table->String('Status',255);
+            $table->timestamps();
+            $table->softDeletes(); // Add this line for soft deletes
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('ECLC_INPUTS', function (Blueprint $table) {
+            $table->dropSoftDeletes(); // This removes the deleted_at column
+        });
+    }
+};
