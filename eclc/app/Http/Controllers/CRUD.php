165a7a1bc2894d4e6eb1ad4043a4433e369c9301
berlily29 <yambaoluise29@gmail.com>
2024-10-03 10:15:16 +0800

    route

--- eclc/app/Http/Controllers/CRUD.php
+++ eclc/app/Http/Controllers/CRUD.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class CRUD extends Controller
 {
-    //
+    public function Dashboard() {
+        return view('ECLC_Dashboard');
+    }
+    public function LISTMOAEP() {
+        return view('ECLC_LIST_MOA_EP');
+    }
+    public function LISTMOA() {
+        return view('ECLC_LIST_MOA');
+    }
+    public function ManageMOA() {
+        return view('ECLC_Manage_MOA');
+    }
+    public function UploadMOAEP() {
+        return view('ECLC_Upload_MOA_EP');
+    }
+    public function UploadMOA() {
+        return view('ECLC_Upload_MOA');
+    }
+    public function UploadNotarized() {
+        return view('ECLC_Upload_Notarized_MOA');
+    }
+    public function ViewMOA() {
+        return view('ECLC_View_MOA');
+    }
 }
--- eclc/routes/web.php
+++ eclc/routes/web.php
@@ -3,12 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CRUD;
 
-Route::group(['middleware' => 'auth', 'prefix' => 'dashboard'], function () {
+Route::group([/*'middleware' => 'auth',*/ 'prefix' => ''], function () {
 
-
-    Route::get('/home', [HomeController::class, 'index']);
-    Route::post('/home', [HomeController::class, 'store']);
-    Route::put('/home/{id}', [HomeController::class, 'update']);
-    Route::delete('/home/{id}', [HomeController::class, 'destroy']);
+    Route::get('/dashboard', [CRUD::class, 'Dashboard']);
+    Route::get('/list-EP', [CRUD::class, 'LISTMOAEP']);
+    Route::get('/list', [CRUD::class, 'LISTMOA']);
+    Route::get('/manage', [CRUD::class, 'ManageMOA']);
+    Route::get('/upload-ep', [CRUD::class, 'UploadMOAEP']);
+    Route::get('/upload', [CRUD::class, 'UploadMOA']);
+    Route::get('/upload-notarized', [CRUD::class, 'UploadNotarized']);
+    Route::get('/view', [CRUD::class, 'ViewMOA']);
 });
 
