cd336a586b371bacea5d260f9f182b9039014444
berlily29 <yambaoluise29@gmail.com>
2024-10-03 09:59:37 +0800

    routing

--- eclc/routes/web.php
+++ eclc/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CRUD;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'auth', 'prefix' => 'dashboard'], function () {
+
+
+    Route::get('/home', [HomeController::class, 'index']);
+    Route::post('/home', [HomeController::class, 'store']);
+    Route::put('/home/{id}', [HomeController::class, 'update']);
+    Route::delete('/home/{id}', [HomeController::class, 'destroy']);
 });
+
