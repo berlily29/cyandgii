eba7e1da0974c0687fe13a66e0217912ecdd5126
berlily29 <yambaoluise29@gmail.com>
2024-10-03 11:45:59 +0800

    yield and routing

--- eclc/routes/web.php
+++ eclc/routes/web.php
@@ -3,6 +3,20 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CRUD;
 
+Route::get('/', function () {
+    $users = [
+        ['name' => 'John', 'age' => 25],
+        ['name' => 'Jane', 'age' => 30],
+        ['name' => 'Cyron', 'age' => 21],
+        ['name' => 'Luise', 'age' => 21],
+        ['name' => 'Kim', 'age' => 22],    
+    ];
+
+    $otherData = 'Table of contents';
+
+    return view('welcome', compact('users','otherData'));
+});
+
 Route::group([/*'middleware' => 'auth',*/ 'prefix' => ''], function () {
 
     Route::get('/dashboard', [CRUD::class, 'Dashboard']);
